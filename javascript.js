f8e3cf0377a74ae16ef7bb5a9a2afa4bcbd6934b
John Doe <johndoe@email.com>
2025-01-03 10:29:10 +0100

    jjj

--- javascript.js
+++ javascript.js
@@ -1,27 +1,17 @@
-let brukerPoeng = 0
-let robotPoeng = 0
-
-function play(value) {
-    const choices = ["stein", "saks", "papir"]
-    let robot = choices[Math.floor(Math.random() * 3)]
-    if (!choices.includes(value)) {
-        console.log("Ikke godkjent valg")
+function dropdown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
     }
-    if (value === robot) {
-        console.log("Uavgjort")
-    }
-    else if (value === "stein" & robot === "saks") {
-        console.log("Du vant")
-    }
-    else if (value === "papir" & robot === "stein") {
-        console.log("Du vant")
-    }
-    else if (value === "saks" & robot === "papir") {
-        console.log("Du vant")
-    }
-    else {
-        console.log("Dataen vant")
-    }
-}
-
-//BENT KODE - SLETT SENERE
+  } 
